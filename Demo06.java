cb08514a2b1520f6f638929433f2368c58cb1e38
deepakbathula7 <deepakbathula39@gmail.com>
2025-01-03 10:59:39 +0530

    welcome message

---
+++ Demo06.java
@@ -0,0 +1,11 @@
+package Demo_java;
+public class Demo06 {
+    public static void main(String[] args) {
+        String name = "Hardik";
+        String pwd = "Pandya";
+        while ((name == "Hardik") && (pwd == "Pandya")) {
+            System.out.println("Hello Hardik welcome to mru");
+            break;
+        }
+    }
+}
