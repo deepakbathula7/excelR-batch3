bfb16b746c7b630534f86545ef1eb33e9ddf377b
deepakbathula7 <deepakbathula39@gmail.com>
2025-01-04 10:06:22 +0530

    Banking system
    Banking system where users can manage their accounts, check balances, and perform transactions

---
+++ Demo009.java
@@ -0,0 +1,29 @@
+package Demo_java;
+
+import java.util.Scanner;
+
+public class Demo009 {
+    String custName = "Deepak";
+    long accountNo = 436286510; 
+    Double balance = 1000000.00; 
+
+    void acceptDetails() {
+        System.out.println("Enter your name and account number:");
+        Scanner sc = new Scanner(System.in);
+        custName = sc.nextLine();
+        accountNo = sc.nextLong();
+        sc.nextLine(); 
+        sc.close();
+    }
+
+    void displayDetails() {
+        System.out.println("Hello " + custName);
+        System.out.println("Your balance is " + balance);
+    }
+
+    public static void main(String[] args) {
+        Demo009 obj = new Demo009(); 
+        obj.acceptDetails(); 
+        obj.displayDetails(); 
+    }
+}
