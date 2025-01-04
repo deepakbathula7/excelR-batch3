f6fab2d361e0ba3094c4f01af5dcebb7e125245a
deepakbathula7 <deepakbathula39@gmail.com>
2025-01-04 09:47:26 +0530

    login system for malla reddy university

---
+++ Demo07.java
@@ -0,0 +1,30 @@
+package Demo_java;
+
+import java.util.Scanner;
+
+public class Demo07 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in); // Create a Scanner object
+        String pwd = ""; // Initialize pwd
+        String loginid = ""; // Initialize loginid
+        String reply;
+
+        do {
+            System.out.println("Welcome to Malla Reddy University");
+            System.out.println("Have you visited earlier? (yes/no)");
+            reply = sc.nextLine();
+
+            if (reply.equals("no")) {
+                System.out.println("Login here");
+                System.out.println("Enter your ID:");
+                loginid = sc.nextLine();
+                System.out.println("Enter your password:");
+                pwd = sc.nextLine();
+            } else {
+                System.out.println("You are our valued customer");
+            }
+        } while (!(loginid.equals("Deepak") && pwd.equals("12345678")));
+        System.out.println("Login successful!");
+        sc.close();
+    }
+}
