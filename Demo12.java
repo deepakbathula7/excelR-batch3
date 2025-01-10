60c1c43b01ebfdb00352f49a6ae457466e459c11
deepakbathula7 <deepakbathula39@gmail.com>
2025-01-10 06:59:58 -0800

    Circle Area

---
+++ Demo12.java
@@ -0,0 +1,12 @@
+class CircleAreaDemo1 {
+   
+    void Area() {
+    }
+
+}
+public class Demo12{
+    public static void main(String[] args){
+        CircleAreaDemo1 obj = new CircleAreaDemo1();
+        obj.Area();
+    }
+}
