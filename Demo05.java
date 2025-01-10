0d4cd8c1468c7f3b26a28c9c086e02d3c6a09b9c
deepakbathula7 <deepakbathula39@gmail.com>
2025-01-10 06:06:58 -0800

    Login page

---
+++ Demo05.java
@@ -0,0 +1,10 @@
+public class Demo05 {
+    public static void main(String[] args) {
+        String username = "user01";
+        String pwd = "user_01";
+        while((username == "user01") && (pwd == "user_01")) {
+            System.out.println("Succesfully login");
+            break;
+        }
+    }
+}
