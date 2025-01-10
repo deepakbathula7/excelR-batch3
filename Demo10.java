89923888a4f021f933dfbf9f405ca616e0c0db5c
deepakbathula7 <deepakbathula39@gmail.com>
2025-01-10 06:47:44 -0800

    Bank system

---
+++ Demo10.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Customer {
+    private String name;
+    private int accnumber;
+    private double balance; 
+    public void acceptDetails() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter customer name: ");
+        name = sc.nextLine();
+        System.out.println("Enter bank account number: ");
+        accnumber = sc.nextInt();
+        System.out.println("Enter the balance: ");
+        balance = sc.nextDouble(); // Changed to nextDouble for balance
+    }
+
+    // Method to display customer details
+    public void displayDetails() {
+        System.out.println("\nDetails:");
+        System.out.println("Name: " + name);
+        System.out.println("Account number: " + accnumber);
+        System.out.println("Balance: " + balance);
+    }
+}
+
+public class Demo10 {
+    public static void main(String[] args) {
+        Customer person = new Customer(); // Changed to uppercase 'Customer'
+        person.acceptDetails();
+        person.displayDetails();
+    }
+}
