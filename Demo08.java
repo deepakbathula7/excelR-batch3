e9732ce099b8653ea0df55e128a5d8c0274502f0
deepakbathula7 <deepakbathula39@gmail.com>
2025-01-10 06:20:02 -0800

    Login details

---
+++ Demo08.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Demo08 {
+    public static void main(String[] args) {
+        String name;
+        int age;
+        long phno;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please enter your name: ");
+        name = sc.nextLine();
+        System.out.println("please enter yoyr age: ");
+        age = sc.nextInt();
+        System.out.println("please enter your phone number: ");
+        phno = sc.nextLong();
+        System.out.println("Hello "+name+" your age is "+age+" your phone nmber is "+phno);
+    }
+}
