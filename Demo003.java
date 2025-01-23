11f57245c5934bd67c6c02627469dfbcddbd98dc
deepakbathula7 <deepakbathula39@gmail.com>
2025-01-23 12:41:31 +0530

    Printing 1-100 nmbers

--- Demo003.java
+++ Demo003.java
@@ -1,5 +1,5 @@
 package Core_Java;
-public class Demo03 {
+public class Demo003 {
     public static void main(String[] args) {
         for (int i = 0;i <= 100; i++) {
             System.out.println(i);
