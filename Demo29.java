4bccb6f84e2b331fd946d1fc176bb7eb92b94fe5
deepakbathula7 <deepakbathula39@gmail.com>
2025-01-23 12:53:32 +0530

    Adding 10 friends name

---
+++ Demo29.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+class ArrayListNames {
+    void addFriendNames() {
+        System.out.println("Names of my friends: ");
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Anitha");
+        arrayList.add("Charita");
+        arrayList.add("Rina");
+        arrayList.add("John");
+        arrayList.add("Mike");
+        arrayList.add("Sarah");
+        arrayList.add("Alice");
+        arrayList.add("Bob");
+        arrayList.add("James");
+        arrayList.add("Emma");
+        
+        // Convert ArrayList to array
+        String[] array = arrayList.toArray(new String[0]);
+        
+        // Display the array
+        for (String i : array) {
+            System.out.println(i);
+        }
+    }
+}
+
+public class Demo29 {
+    public static void main(String[] args) {
+        ArrayListNames obj = new ArrayListNames();
+        obj.addFriendNames();
+    }
+}
